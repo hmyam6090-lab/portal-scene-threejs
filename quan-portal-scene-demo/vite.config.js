0135ca1741e3b7e33f89da920a8e74ff676882c8
Yam <hmyam6090@gmail.com>
2026-03-09 05:40:36 -0400

    chore: rebuild project with GitHub Pages base path

--- quan-portal-scene-demo/vite.config.js
+++ quan-portal-scene-demo/vite.config.js
@@ -4,7 +4,7 @@ import glsl from 'vite-plugin-glsl'
 export default {
     root: 'src/',
     publicDir: '../static/',
-    base: './',
+    base: '/portal-scene-threejs/',
     server:
     {
         host: true, // Open to local network and display URL
